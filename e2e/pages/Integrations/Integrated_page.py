5a5265dda1fe8d2af8b6cf1cbd2fbf9db0d73c92
jayeeed <jayedbinjahangir@gmail.com>
2024-11-17 12:09:47 +0600

    new

---
+++ e2e/pages/Integrations/Integrated_page.py
@@ -0,0 +1,35 @@
+from playwright.sync_api import Page
+
+
+class IntegratedPage:
+    SELECTORS = {
+        "integration_nav": '//*[@id="root"]/div[1]/div/div/div[2]/nav/div[2]/div/button[7]',
+        "integrations_card": "//div[2]/div/div[2]/div/div",
+        "three_dot": 'svg path[d="M3 10a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM8.5 10a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM15.5 8.5a1.5 1.5 0 100 3 1.5 1.5 0 000-3z"]',
+        "integration_menu_delete": "//span[contains(text(), 'Delete')]",
+        "integration_disconnect": '[data-testid="button-element"]:text("Disconnect")',
+        # "integration_type": "Type 'DELETE' Here",
+        "integration_delete": '[data-testid="button-element"]:text("Delete")',
+    }
+
+    def __init__(self, page: Page):
+        self.page = page
+
+    def goto_integrations(self):
+        self.page.locator(self.SELECTORS["integration_nav"]).click()
+
+    def delete_integrations(self):
+        self.page.wait_for_timeout(10000)
+        integration_boxes = self.page.locator(self.SELECTORS["integrations_card"]).all()
+
+        for box in integration_boxes:
+            if "Salla Livechat" in box.inner_text():
+                box.locator(self.SELECTORS["three_dot"]).click()
+                self.page.locator(self.SELECTORS["integration_menu_delete"]).click()
+                self.page.locator(self.SELECTORS["integration_disconnect"]).click()
+                box.locator(self.SELECTORS["three_dot"]).click()
+                self.page.locator(self.SELECTORS["integration_menu_delete"]).click()
+                self.page.get_by_placeholder("Type 'DELETE' Here").fill("delete")
+                self.page.locator(self.SELECTORS["integration_delete"]).click()
+        else:
+            assert False, "No Salla integration found"
--- e2e/pages/Integrations/integrations_page.py
+++ e2e/pages/Integrations/integrations_page.py
@@ -2,84 +2,71 @@ from playwright.sync_api import Page
 
 
 class IntegrationsPage:
+    SELECTORS = {
+        "integration_nav": '//*[@id="root"]/div[1]/div/div/div[2]/nav/div[2]/div/button[7]',
+        "integrations_div": 'text="Available Integrations"',
+        ################################################
+        "woocommerce": 'h3:text("WooCommerce")',
+        "shopify": 'h3:text("Shopify")',
+        "salla": 'h3:text("Salla")',
+        "zid": 'h3:text("Zid")',
+        "fb_feed": 'h3:text("Facebook Feed")',
+        "fb_chat": 'h3:text("Messenger")',
+        "ig_feed": 'h3:text("Instagram Feed")',
+        "ig_chat": 'h3:text("Instagram Chat")',
+        "whatsapp": 'h3:text("WhatsApp")',
+        "email": 'h3:text("Email")',
+        "viber": 'h3:text("Viber")',
+        "telegram": 'h3:text("Telegram")',
+        "line": 'h3:text("Line")',
+        "live_chat": 'h3:text("Live Chat Plugin")',
+        "mobile_app": 'h3:text("Mobile App Plugin")',
+        "wit": 'h3:text("Wit.ai")',
+        "checkout": 'h3:text("Checkout.com")',
+        "moengage": 'h3:text("MoEngage")',
+        ################################################
+        "store_continue": '[data-testid="button-element"]',
+        "channel_next": '[data-testid="button-element"]:text("Next")',
+        "wa_next": '[data-testid="button-element"]:text("Connect Whatsapp")',
+        "channel_continue": 'button:text("Connect With")',
+        "close_icon": 'svg path[d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"]',
+        "close_toast": 'svg path[d="M18 6 6 18"]',
+    }
+
     def __init__(self, page: Page):
         self.page = page
 
-        self.selectors = {
-            "integration_nav": '//*[@id="root"]/div[1]/div/div/div[2]/nav/div[2]/div/button[7]',
-            "integrations_div": '//span[contains(text(), "Available Integrations")]',
-            ######### Integrations #########
-            "woocommerce": '//h3[contains(text(), "WooCommerce")]',
-            "shopify": '//h3[contains(text(), "Shopify")]',
-            "salla": '//h3[contains(text(), "Salla")]',
-            "zid": '//h3[contains(text(), "Zid")]',
-            "fb_feed": '//h3[contains(text(), "Facebook Feed")]',
-            "fb_chat": '//h3[contains(text(), "Messenger")]',
-            "ig_feed": '//h3[contains(text(), "Instagram Feed")]',
-            "ig_chat": '//h3[contains(text(), "Instagram Chat")]',
-            "whatsapp": '//h3[contains(text(), "WhatsApp")]',
-            "email": '//h3[contains(text(), "Email")]',
-            "viber": '//h3[contains(text(), "Viber")]',
-            "telegram": '//h3[contains(text(), "Telegram")]',
-            "line": '//h3[contains(text(), "Line")]',
-            "live_chat": '//h3[contains(text(), "Live Chat Plugin")]',
-            "mobile_app": '//h3[contains(text(), "Mobile App Plugin")]',
-            "wit": '//h3[contains(text(), "Wit.ai")]',
-            "checkout": '//h3[contains(text(), "Checkout.com")]',
-            "moengage": '//h3[contains(text(), "MoEngage")]',
-            ######### Integrations #########
-            "store_continue": '[data-testid="button-element"]',
-            "channel_next": '[data-testid="button-element"]:text("Next")',
-            "wa_next": '[data-testid="button-element"]:text("Connect Whatsapp")',
-            "channel_continue": '//button[contains(text(), "Connect With")]',
-            "close_icon": "path[d='M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z']",
-            "close_toast": "path[d='M18 6 6 18']",
-        }
-
     def goto_integrations(self):
-        self.page.wait_for_selector(
-            self.selectors["integration_nav"], state="visible"
-        ).click()
-        self.page.wait_for_selector(
-            self.selectors["integrations_div"], state="visible"
-        ).click()
+        self.page.locator(self.SELECTORS["integration_nav"]).click()
+        self.page.locator(self.SELECTORS["integrations_div"]).click()
 
     def goto_woocommerce_shopify_zid(self, integration, url):
-        self.page.click(self.selectors[integration])
-        self.page.wait_for_selector(
-            self.selectors["store_continue"], state="visible"
-        ).click()
+        self.page.locator(self.SELECTORS[integration]).click()
+        self.page.locator(self.SELECTORS["store_continue"]).click()
 
         new_tab = self.page.context.wait_for_event("page")
         new_tab.wait_for_load_state("domcontentloaded")
         assert url in new_tab.url
         new_tab.close()
-        self.page.locator(self.selectors["close_icon"]).click()
+        self.page.locator(self.SELECTORS["close_icon"]).click()
 
     def goto_salla(self):
-        self.page.click(self.selectors["salla"])
-        self.page.wait_for_selector(
-            self.selectors["store_continue"], state="visible"
-        ).click()
-        self.page.locator(self.selectors["close_icon"]).click()
-        self.page.wait_for_selector(self.selectors["close_toast"]).click()
+        self.page.locator(self.SELECTORS["salla"]).click()
+        self.page.locator(self.SELECTORS["store_continue"]).click()
+        self.page.locator(self.SELECTORS["close_icon"]).click()
+        self.page.locator(self.SELECTORS["close_toast"]).click()
 
     def goto_meta(self, integration, url):
-        parent_page = self.page
-        parent_page.click(self.selectors[integration])
+        self.page.locator(self.SELECTORS[integration]).click()
 
         if integration == "whatsapp":
-            parent_page.wait_for_selector(
-                self.selectors["wa_next"], state="visible"
-            ).click()
+            self.page.locator(self.SELECTORS["wa_next"]).click()
         else:
-            parent_page.wait_for_selector(
-                self.selectors["channel_next"], state="visible"
-            ).click()
-            parent_page.click(self.selectors["channel_continue"])
+            self.page.locator(self.SELECTORS["channel_next"]).click()
+            self.page.locator(self.SELECTORS["channel_continue"]).click()
 
         child_page = self.page.context.wait_for_event("page")
         child_page.wait_for_load_state("domcontentloaded")
         assert url in child_page.url
         child_page.close()
-        self.page.locator(self.selectors["close_icon"]).click()
+        self.page.locator(self.SELECTORS["close_icon"]).click()
